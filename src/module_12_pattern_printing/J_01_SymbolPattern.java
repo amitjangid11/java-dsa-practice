016eaa080b7ee6075141614e428a5e7e1c2ce3ec
amitjangid11 <amitjangid.aj11@gmail.com>
2025-09-26 21:05:04 +0530

    New Symbol pattern added

--- src/module_12_pattern_printing/J_01_SymbolPattern.java
+++ src/module_12_pattern_printing/J_01_SymbolPattern.java
@@ -2,18 +2,32 @@ package module_12_pattern_printing;
 
 public class J_01_SymbolPattern {
     public static void main(String[] args) {
-        rectanglePattern(4);
-        squarePattern(4);
-        starRightTriPattern(4);
+//        rectanglePattern(4);
+//        squarePattern(4);
+//        starRightTriPattern(4);
+//        reverseStarRightTriPattern(4);
+
     }
 
-    // rectangle
-    static void rectanglePattern(int n) {
-        System.out.println("1. Rectangle Pattern");
+    // Reverse starRightTriPattern
+    static void reverseStarRightTriPattern(int n) {
+        System.out.println("4. Reverse Star Right Triangle");
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 5; j++) {
+            for (int j = i; j < n; j++) {
                 System.out.print("* ");
             }
+            System.out.println();
+        }
+    }
+
+    // starRightTriPattern
+    static void starRightTriPattern(int n) {
+        System.out.println("3. Star Right Triangle");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
     }
 
@@ -28,14 +42,13 @@ public class J_01_SymbolPattern {
         }
     }
 
-    static void starRightTriPattern(int n) {
-        System.out.println("3. Star Right Triangle");
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
+    // rectangle
+    static void rectanglePattern(int n) {
+        System.out.println("1. Rectangle Pattern");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 5; j++) {
                 System.out.print("* ");
             }
-            System.out.println();
         }
     }
-
 }
