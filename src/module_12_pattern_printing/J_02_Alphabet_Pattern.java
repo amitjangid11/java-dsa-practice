c662321350ba030e09f9f8cfd91603a226e9015e
amitjangid11 <amitjangid.aj11@gmail.com>
2025-10-02 21:00:56 +0530

    Alphabet Pattern

--- src/module_12_pattern_printing/J_02_Alphabet_Pattern.java
+++ src/module_12_pattern_printing/J_02_Alphabet_Pattern.java
@@ -1,4 +1,23 @@
 package module_12_pattern_printing;
 
+import java.util.Scanner;
+
 public class J_02_Alphabet_Pattern {
+    public static void main(String[] args) {
+        System.out.print("Enter n : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        alphabetRectangle(n);
+    }
+
+    static void alphabetRectangle(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+//                char ch = (char)(i + 65);
+                char ch = (char)(j + 65);
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
 }
