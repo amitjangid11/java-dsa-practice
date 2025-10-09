9cb40b6acab55c9d52bbdd5c4e7c1c154f054c3a
amitjangid11 <amitjangid.aj11@gmail.com>
2025-10-10 00:28:26 +0530

    New Numeric Pattern Added

--- src/module_12_pattern_printing/J_03_Numeric_Pattern.java
+++ src/module_12_pattern_printing/J_03_Numeric_Pattern.java
@@ -1,4 +1,66 @@
 package module_12_pattern_printing;
 
 public class J_03_Numeric_Pattern {
+    public static void main(String[] args) {
+//        numberSquare(5);
+//        numberTriangle(5);
+//        floydsTriangle(5);
+//        oddNumberTri2(5);
+    }
+
+    // oddTriangle : M-2
+    public static void oddNumberTri2(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= 2 * i + 1; j+=2) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // oddTriangle : M-1
+    public static void oddNumberTri(int n) {
+        for (int i = 0; i < n; i++) {
+            int a = 1;
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(a + " ");
+                a += 2;
+            }
+            System.out.println();
+        }
+    }
+
+    // floydsTriangle
+    public static void floydsTriangle(int n) {
+        int a = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(a + " ");
+                a++;
+            }
+            System.out.println();
+        }
+    }
+
+    // numberTriangle
+    public static void numberTriangle(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+//                System.out.print(i+1+" ");
+                System.out.print(j + 1 + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // numberSquare
+    public static void numberSquare(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+//                System.out.print(i+1 + " ");
+                System.out.print(j + 1 + " ");
+            }
+            System.out.println();
+        }
+    }
 }
